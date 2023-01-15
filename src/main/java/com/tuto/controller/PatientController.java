8366a9e5a9c4abbabbadf1ad28efdf56e639d0b9
Gandouz <gan.developer@gmail.com>
2023-01-15 16:49:20 +0100

    Commit to develop

--- src/main/java/com/tuto/controller/PatientController.java
+++ src/main/java/com/tuto/controller/PatientController.java
@@ -1,16 +1,22 @@
 package com.tuto.controller;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tuto.entities.Patient;
 import com.tuto.services.IPatientService;
 
 @RestController
+@RequestMapping("/api/v1")
 public class PatientController {
 	
 	@Autowired
@@ -24,5 +30,11 @@ public class PatientController {
 	@GetMapping("/patients/{id}")
 	public Patient retrievePatient(@PathVariable Long id){
 		return patientService.findPatientById(id);
+	}
+	
+	@PutMapping("/something")
+	public void handle(@RequestBody String body, Writer writer) throws IOException {
+		  writer.write("body received:\n");
+		  writer.write(body);
 	}
 }
